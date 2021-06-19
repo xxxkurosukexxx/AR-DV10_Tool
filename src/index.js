4dc58736c5b788fb31c056ae862ae213598498b5
xxxkurosukexxx <info@kurosuke.org>
2021-06-19 23:56:28 +0900

    add MemoryChannel (Load)

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ ws.onmessage = (e) => {
 
     if (e.data.startsWith('20MW')) {
         setMemoryBankTable(e.data);
+    } else if (e.data.startsWith('21MX') || e.data.startsWith('20MX')) {
+        setMemoryChannelTable(e.data);
     }
 }
 
@@ -40,6 +42,75 @@ function getProtectHtml(protect) {
     return _protect;
 }
 
+function getPassHtml(pass) {
+    let _pass;
+    switch (pass) {
+        case '0':
+            _pass = '<span uk-icon="minus"></span>';
+            break;
+        case '1':
+            _pass = '<span uk-icon="check"></span>';
+            break;
+    }
+    return _pass;
+}
+
+function getModeText(mode) {
+    let _mode;
+    switch (mode.substr(1, 1)) {
+        case 'F':
+            switch (mode.substr(2, 1)) {
+                case '0':
+                    _mode = 'FM';
+                    break;
+                case '1':
+                    _mode = 'AM';
+                    break;
+                case '2':
+                    _mode = 'SAH';
+                    break;
+                case '3':
+                    _mode = 'SAL';
+                    break;
+                case '4':
+                    _mode = 'USB';
+                    break;
+                case '5':
+                    _mode = 'LSB';
+                    break;
+                case '6':
+                    _mode = 'CW';
+                    break;
+            }
+            break;
+        case '0':
+            _mode = 'AUTO';
+            break;
+        case '1':
+            _mode = 'D-STAR';
+            break;
+        case '2':
+            _mode = 'YAESU';
+            break;
+        case '3':
+            _mode = 'ALINCO';
+            break;
+        case '4':
+            _mode = 'D-CR/NXDN';
+            break;
+        case '5':
+            _mode = 'P25';
+            break;
+        case '6':
+            _mode = 'dPMR';
+            break;
+        case '7':
+            _mode = 'DMR';
+            break;
+    }
+    return _mode;
+}
+
 // 
 // ----- MemoryBank -----
 // 
@@ -62,6 +133,54 @@ function setMemoryBankTable(msg) {
 }
 
 function saveMemoryBank() {
+    // TODO
+    return false;
+}
+
+// 
+// ----- MemoryChannel -----
+// 
+async function loadMemoryChannel() {
+    let _bank = $('#memoryChannelBankSelect').val();
+    for (let i = 0; i <= 49; i++) {
+        send2('MA' + _bank + ('0' + i).substr(-2));
+        await _sleep(200);
+    }
+    return false;
+}
+
+function setMemoryChannelTable(msg) {
+    let _number, _pass, _receiveFrequency, _adjustFrequency, _stepAdjustFrequency, _mode, _protect, _title, _$tr;
+    if (msg.split(' ')[1] == '---') {
+        _number = msg.substr(6, 2);
+        _pass = '';
+        _receiveFrequency = '---';
+        _adjustFrequency = '';
+        _stepAdjustFrequency = '';
+        _mode = '';
+        _protect = '';
+        _title = '';
+    } else {
+        _number = msg.substr(6, 2);
+        _pass = getPassHtml(msg.substr(11, 1));
+        _receiveFrequency = msg.substr(15, 10) + ' MHz';
+        _adjustFrequency = msg.substr(28, 6) + ' kHz';
+        _stepAdjustFrequency = msg.substr(37, 6) + ' kHz';
+        _mode = getModeText(msg.substr(46, 3));
+        _protect = getProtectHtml(msg.substr(52, 1));
+        _title = msg.substr(56);
+    }
+    _$tr = $('#memoryChannelTable tbody').find('tr[data-memory-channel-number="' + _number + '"]');
+    _$tr.find('td[data-for="channel-pass"]').html(_pass);
+    _$tr.find('td[data-for="channel-receive-frequency"]').text(_receiveFrequency);
+    _$tr.find('td[data-for="channel-adjust-frequency"]').text(_adjustFrequency);
+    _$tr.find('td[data-for="channel-step-adjust-frequency"]').text(_stepAdjustFrequency);
+    _$tr.find('td[data-for="channel-mode"]').text(_mode);
+    _$tr.find('td[data-for="channel-protect"]').html(_protect);
+    _$tr.find('td[data-for="channel-title"]').text(_title);
+}
+
+function saveMemoryChannel() {
     // TODO
     return false;
 }
